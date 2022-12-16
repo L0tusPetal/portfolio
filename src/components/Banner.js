85024d7d919ff476af418a5e225197782842a2f6
Eve Frances <evefrances92@icloud.com>
2022-12-15 19:40:34 -0500

    removed an unused variable

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
-import { ArrowRightCircle, Quote } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
