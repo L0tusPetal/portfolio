f44a008ca178d04bc6e123657c3be390b318230f
Eve Frances <evefrances92@icloud.com>
2022-12-31 13:41:17 -0500

    made the email link into a button - still needs color editing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 // import navIcon1 from "../assets/img/discord.svg";
 // import navIcon2 from "../assets/img/github.svg";
 import 'animate.css';
+import Button from '@mui/material/Button';
+
 
 export const Footer = () => {
   return (
@@ -10,13 +12,8 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={200} className="text-center text-sm-center">
-            {/* <div className="social-icon">
-                <a href="https://www.linkedin.com/in/wrenfrances/" target="_blank" rel="noreferrer"><img src={navIcon1} alt="LinkedIn" /></a>
-                <a href="https://github.com/L0tusPetal"target="_blank" rel="noreferrer"><img src={navIcon2} alt="Github" /></a>
-                
-            </div> */}
             <div>
-            <a href="mailto:wrenfrances92@gmail.com">Email Me!</a>
+            <Button variant="outlined"><a href="mailto:wrenfrances92@gmail.com">Email Me!</a></Button>
             </div>
             <p>Made with ❤️ NodeX | WannaCry</p>
           </Col>
