38fa1342ca2caeb18cd4e8118fcc376bf82bef61
Eve Frances <evefrances92@icloud.com>
2023-01-03 09:30:13 -0500

    removed unused files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Container, Row, Col } from "react-bootstrap";
-// import navIcon1 from "../assets/img/discord.svg";
-// import navIcon2 from "../assets/img/github.svg";
 import 'animate.css';
 import Button from '@mui/material/Button';
 import { deepPurple } from '@mui/material/colors';
